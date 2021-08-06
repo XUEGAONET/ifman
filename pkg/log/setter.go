66e46a618230b37a1a06c7b0b33b4fbb16da879c
ic0xgkk <ic0xgkk@gmail.com>
2021-08-06 16:23:02 +0800

    update log formatter to text

--- pkg/log/setter.go
+++ pkg/log/setter.go
@@ -17,14 +17,32 @@ func SetLog(level string, w Writer, useSyslog bool) error {
 
 	logrus.SetLevel(logrus.Level(logrusLevel))
 
-	logrus.SetFormatter(&logrus.JSONFormatter{
-		TimestampFormat:   "2006-01-02 15:04:05",
-		DisableTimestamp:  false,
-		DisableHTMLEscape: true,
-		DataKey:           "",
-		FieldMap:          nil,
-		CallerPrettyfier:  nil,
-		PrettyPrint:       false,
+	//logrus.SetFormatter(&logrus.JSONFormatter{
+	//	TimestampFormat:   "2006-01-02 15:04:05",
+	//	DisableTimestamp:  false,
+	//	DisableHTMLEscape: true,
+	//	DataKey:           "",
+	//	FieldMap:          nil,
+	//	CallerPrettyfier:  nil,
+	//	PrettyPrint:       false,
+	//})
+
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors:               false,
+		DisableColors:             false,
+		ForceQuote:                false,
+		DisableQuote:              false,
+		EnvironmentOverrideColors: false,
+		DisableTimestamp:          false,
+		FullTimestamp:             false,
+		TimestampFormat:           "2006-01-02 15:04:05",
+		DisableSorting:            false,
+		SortingFunc:               nil,
+		DisableLevelTruncation:    false,
+		PadLevelText:              false,
+		QuoteEmptyFields:          false,
+		FieldMap:                  nil,
+		CallerPrettyfier:          nil,
 	})
 
 	logrus.SetOutput(w.GetWriter())
